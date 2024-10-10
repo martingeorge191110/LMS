5d1331bf5deee1275c522bcf8a9dd6900f604f6c
Martin George <martingeorge191110@gmail.com>
2024-10-10 14:44:42 +0300

    feat: (--> login Validators, --> resetPass Validators)

--- utilies/authValidator.js
+++ utilies/authValidator.js
@@ -25,16 +25,22 @@ class AuthValidation {
       return (new AuthValidation(true, "No Messages"))
    }
 
-   /* Check Whether neccessary fields for user Registeration is Valid */
-   static registerValid (fn, ln, email, password, country) {
-      const fnValid = AuthValidation.nameValidation(fn)
-      const lnValid = AuthValidation.nameValidation(ln)
-      const emailValid = validator.isEmail(email)
+   /* Check about whether user password is strong or not */
+   static isStrongPassword = (password) => {
       const strongPass = validator.isStrongPassword(password, {
          minLength: 8, minLowercase: 1, minUppercase: 0, 
          minNumbers: 0, minSymbols: 0
       })
-      const countryValid = validator.isEmpty(country)
+
+      return (strongPass)
+   }
+   /* Check Whether neccessary fields for user Registeration is Valid */
+   static registerValid = (fn, ln, email, password) => {
+      const fnValid = AuthValidation.nameValidation(fn)
+      const lnValid = AuthValidation.nameValidation(ln)
+      const emailValid = validator.isEmail(email)
+      const strongPass = this.isStrongPassword(password)
+
       if (!fnValid.success)
          return (fnValid)
       else if (!lnValid.success)
@@ -43,9 +49,38 @@ class AuthValidation {
          return (new AuthValidation(false, "Email is not Valid!"))
       else if (!strongPass)
          return (new AuthValidation(false, "Password is Not strong Enough!"))
-      else if (countryValid)
-         return (new AuthValidation(false, "Country field should not be Empty!"))
 
-      return (new AuthValidation(true, "Register fields is Valid, you can go to next step!"))
+      return (new AuthValidation(true, "Register fields is Valid, you can go to the next step!"))
+   }
+
+   /* Check Whether neccessary fields for user Login is Valid */
+   static loginValid = (email, password) => {
+      const emailValid = validator.isEmail(email)
+      const password = validator.isEmpty(password)
+      if (!emailValid)
+         return (new AuthValidation(false, "Email is not Valid!"))
+      else if (password)
+         return (new AuthValidation(false, "Password Field is Empty, please enter your Password!"))
+
+      return (new AuthValidation(true, "Login fields is Valid, you can go to the next step!"))
+   }
+
+   /* Check Whether neccessary fields for user resetPass is Valid */
+   static resetPassValid = (email, password, confirmPass) => {
+      const isEmail = validator.isEmail(email)
+      const isStrongPassword = this.isStrongPassword(password)
+      const isPassEqConPass = password === confirmPass ? true : false
+
+      if (!isEmail)
+         return (new AuthValidation(false, "Email is not Valid!"))
+      else if (!isStrongPassword)
+         return (new AuthValidation(false, "Password Field is not storng!"))
+      else if (!isPassEqConPass)
+         return (new AuthValidation(false, "Confirm password field is not Equall password!"))
+
+      return (new AuthValidation(true, "Every thing is Valid, got to login process using new Password!"))
    }
 }
+
+
+export default AuthValidation;
